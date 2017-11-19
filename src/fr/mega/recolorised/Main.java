cd03e7ee24a64d9566d6d129d0f1fb9564ccf7b8
Gaëtan Bessot <gaetanbessot.officiel@gmail.com>
2017-11-19 23:00:30 +0100

    Merge functions

--- src/fr/mega/recolorised/Main.java
+++ src/fr/mega/recolorised/Main.java
@@ -15,9 +15,9 @@ public class Main extends JavaPlugin {
         for(short i = 0; i < 16; i++) {
             for(short j = 0; j < 16; j++) {
                 if(i != j) {
-                    setHCCraft(i, j);
-                    setSGCraft(i, j);
-                    setGPCraft(i, j);
+                    setRecoloringCraft(Material.STAINED_CLAY, i, j);
+                    setRecoloringCraft(Material.STAINED_GLASS, i, j);
+                    setRecoloringCraft(Material.STAINED_GLASS_PANE, i, j);
                 }
             }
         }
@@ -29,39 +29,15 @@ public class Main extends JavaPlugin {
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "[Recolorised] Disabled.");
     }
 
-    public void setHCCraft(short srcColor, short dstColor) {
-        ItemStack resultat = new ItemStack(Material.STAINED_CLAY, 8);
+    public void setRecoloringCraft(Material mat, short srcColor, short dstColor) {
+        ItemStack resultat = new ItemStack(mat, 8);
         resultat.setDurability((short) (15-dstColor));
 
-        ShapedRecipe hcColored = new ShapedRecipe(new ItemStack(resultat));
-        hcColored.shape("SSS", "SDS", "SSS");
-        hcColored.setIngredient('S', Material.STAINED_CLAY, abs(srcColor-15));
-        hcColored.setIngredient('D', Material.INK_SACK, dstColor);
+        ShapedRecipe recolored = new ShapedRecipe(new ItemStack(resultat));
+        recolored.shape("SSS", "SDS", "SSS");
+        recolored.setIngredient('S', mat, abs(srcColor-15));
+        recolored.setIngredient('D', Material.INK_SACK, dstColor);
 
-        getServer().addRecipe(hcColored);
-    }
-
-    public void setSGCraft(short srcColor, short dstColor) {
-        ItemStack resultat = new ItemStack(Material.STAINED_GLASS, 8);
-        resultat.setDurability((short) (15-dstColor));
-
-        ShapedRecipe sgColored = new ShapedRecipe(new ItemStack(resultat));
-        sgColored.shape("SSS", "SDS", "SSS");
-        sgColored.setIngredient('S', Material.STAINED_GLASS, abs(srcColor-15));
-        sgColored.setIngredient('D', Material.INK_SACK, dstColor);
-
-        getServer().addRecipe(sgColored);
-    }
-
-    public void setGPCraft(short srcColor, short dstColor) {
-        ItemStack resultat = new ItemStack(Material.STAINED_GLASS_PANE, 8);
-        resultat.setDurability((short) (15-dstColor));
-
-        ShapedRecipe gpColored = new ShapedRecipe(new ItemStack(resultat));
-        gpColored.shape("SSS", "SDS", "SSS");
-        gpColored.setIngredient('S', Material.STAINED_GLASS_PANE, abs(srcColor-15));
-        gpColored.setIngredient('D', Material.INK_SACK, dstColor);
-
-        getServer().addRecipe(gpColored);
+        getServer().addRecipe(recolored);
     }
 }
