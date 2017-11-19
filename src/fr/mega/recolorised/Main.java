cbd8e69565cb3410b9ea87cefa81fe0b8d9dd131
Gaëtan Bessot <gaetanbessot.officiel@gmail.com>
2017-11-19 21:05:52 +0100

    Add glass blocks and glass panes

---
+++ src/fr/mega/recolorised/Main.java
@@ -0,0 +1,67 @@
+package fr.mega.recolorised;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import static java.lang.Math.abs;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        for(short i = 0; i < 16; i++) {
+            for(short j = 0; j < 16; j++) {
+                if(i != j) {
+                    setHCCraft(i, j);
+                    setSGCraft(i, j);
+                    setGPCraft(i, j);
+                }
+            }
+        }
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Recolorised] Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[Recolorised] Disabled.");
+    }
+
+    public void setHCCraft(short srcColor, short dstColor) {
+        ItemStack resultat = new ItemStack(Material.STAINED_CLAY, 8);
+        resultat.setDurability((short) (15-dstColor));
+
+        ShapedRecipe hcColored = new ShapedRecipe(new ItemStack(resultat));
+        hcColored.shape("SSS", "SDS", "SSS");
+        hcColored.setIngredient('S', Material.STAINED_CLAY, abs(srcColor-15));
+        hcColored.setIngredient('D', Material.INK_SACK, dstColor);
+
+        getServer().addRecipe(hcColored);
+    }
+
+    public void setSGCraft(short srcColor, short dstColor) {
+        ItemStack resultat = new ItemStack(Material.STAINED_GLASS, 8);
+        resultat.setDurability((short) (15-dstColor));
+
+        ShapedRecipe hcColored = new ShapedRecipe(new ItemStack(resultat));
+        hcColored.shape("SSS", "SDS", "SSS");
+        hcColored.setIngredient('S', Material.STAINED_GLASS, abs(srcColor-15));
+        hcColored.setIngredient('D', Material.INK_SACK, dstColor);
+
+        getServer().addRecipe(hcColored);
+    }
+
+    public void setGPCraft(short srcColor, short dstColor) {
+        ItemStack resultat = new ItemStack(Material.STAINED_GLASS_PANE, 8);
+        resultat.setDurability((short) (15-dstColor));
+
+        ShapedRecipe hcColored = new ShapedRecipe(new ItemStack(resultat));
+        hcColored.shape("SSS", "SDS", "SSS");
+        hcColored.setIngredient('S', Material.STAINED_GLASS_PANE, abs(srcColor-15));
+        hcColored.setIngredient('D', Material.INK_SACK, dstColor);
+
+        getServer().addRecipe(hcColored);
+    }
+}
